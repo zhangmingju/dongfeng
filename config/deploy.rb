e3fac1d1de49fd723b04a687f5b1dbaf6b90b6ba
dongfeng <1417278284@qq.com>
2017-03-30 20:11:32 +0800

    add deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,45 @@
 require 'mina/rails'
 require 'mina/git'
-# require 'mina/rbenv'  # for rbenv support. (https://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (https://rvm.io)
+require 'mina/bundler'
+require 'mina/rvm'
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
-set :application_name, 'foobar'
-set :domain, 'foobar.com'
-set :deploy_to, '/var/www/foobar.com'
-set :repository, 'git://...'
+set :application_name, 'dongfeng'
+set :domain, 'bajiudongfeng.xyz'
+set :deploy_to, '/home/zhang/dongfeng'
+set :repository, 'https://github.com/zhangmingju/dongfeng.git'
 set :branch, 'master'
+set :user, 'zhang'
+set :rvm_path, '/home/zhang/.rvm/bin/rvm'
 
-# Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
-#   set :port, '30000'           # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
 # set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+set :shared_dirs, fetch(:shared_dirs, []).push("log")
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
+
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
+  invoke :'rvm:use', 'ruby-2.3.0@global'
 end
-
 # Put any custom commands you need to run at setup
 # All paths in `shared_dirs` and `shared_paths` will be created on their own.
 task :setup do
-  # command %{rbenv install 2.3.0}
+
+  command "mkdir -p #{fetch(:deploy_to)}/shared/log"
+  command "chmod g+rx,u+rwx #{fetch(:deploy_to)}/shared/log"
+
+  command %[mkdir -p "#{fetch(:deploy_to)}/shared/config"]
+  command %[chmod g+rx,u+rwx "#{fetch(:deploy_to)}/shared/config"]
+
+  command %[touch "#{fetch(:deploy_to)}/shared/config/database.yml"]
+  command %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+
+  command %[touch "#{fetch(:deploy_to)}/shared/config/secrets.yml"]
+  command %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
+
 end
 
 desc "Deploys the current version to the server."
@@ -48,25 +49,25 @@ task :deploy do
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
+    # command "gem install bundler"
+    # command "gem update bundler"
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
+    command "bundle install"
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
-    on :launch do
-      in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
-      end
-    end
+
+    # on :launch do
+    #   in_path(fetch(:current_path)) do
+    #     command %{mkdir -p tmp/}
+    #     command %{touch tmp/restart.txt}
+    #   end
+    # end
   end
 
   # you can use `run :local` to run tasks on local machine before of after the deploy scripts
   # run(:local){ say 'done' }
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - https://github.com/mina-deploy/mina/tree/master/docs
