33342843c25df6f982459008b5a48a1d5d89afe5
zmj <dongfeng@dongfengdeMacBook-Pro.local>
2019-08-22 15:52:51 +0800

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,10 @@
 redis_passwd = eval("Settings.redis.#{Rails.env[0,3]}.password")
 redis_url = "redis://:#{redis_passwd}@#{Settings.redis.host}:#{Settings.redis.port}/#{Settings.redis.db}"
 
-$redis = Redis.new(:host=>Settings.redis.host,:port=>Settings.redis.port,:db=>Settings.redis.db,:password=>redis_passwd)
+$redis = Redis.new(host: Settings.redis.host,
+                   port: Settings.redis.port,
+                   db: Settings.redis.db,
+                   password: redis_passwd)
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url, namespace:Settings.redis.namespace.sidekiq }
